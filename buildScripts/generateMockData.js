472efaefd35aa302c7e05746619f5b5834b8759f
Louima Dieubon <louimadieubon@Louimas-MacBook-Pro.local>
2022-07-22 19:32:56 -0400

    adding build configuration

---
+++ buildScripts/generateMockData.js
@@ -0,0 +1,17 @@
+/*eslint-disable no-console */
+
+import { generate, extend } from "json-schema-faker";
+import { schema } from "./mockDataSchema";
+import fs from "fs";
+import chalk from "chalk";
+
+extend("faker", () => require("faker"));
+const json = JSON.stringify(generate(schema));
+
+fs.writeFileSync("./src/api/db.json", json, function (err) {
+    if (err) {
+        return console.log(chalk.red(err));
+    } else {
+        console.log(chalk.green("Mock data generated"));
+    }
+});
--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import numeral from "numeral";
 import "./index.css";
+import { deleteUser, getUser } from "./api/userApi";
 
 /*eslint-disable no-console */
-const courseValue = numeral(1000).format("$0,0.00");
+getUser().then((result) => {
+    let usersBody = "";
+    result.forEach((user) => {
+        usersBody += `<tr>
+<td><a href='#' data-id = "${user.id}" class="deleteUser">Delete</a</td>
+<td>${user.id}</td>
+<td>${user.firstname}</td>
+<td>${user.lastname}</td>
+<td>${user.email}</td>
+</tr>`;
+    });
+    global.document.getElementById("users").innerHTML = usersBody;
+    const deleteLinks = global.document.getElementsByTagName("deleteUser");
 
-console.log(`I would pay ${courseValue} for this awesome course`);
-
-console.log("hello world");
+    Array.from(deleteLinks, (link) => {
+        link.onClick = function (event) {
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes["data-id".value]);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
+});
